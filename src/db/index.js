5e29030bcbd83fd79d059a063df2aa9d89bdec38
manuch7728 <chauhanmanucr7@gmail.com>
2025-08-05 00:16:40 +0530

    mongoDB connection is successfully stablished

--- src/db/index.js
+++ src/db/index.js
@@ -1,17 +1,16 @@
 import mongoose from "mongoose"; // Import mongoose to use it
 
-import { DB_NAME } from "../constants.js";
-
 const connectDB = async () => {
     try {
         // Connect to MongoDB using mongoose and your DB_NAME
-        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+        console.log(process.env.DBurl)
+        const connectionInstance = await mongoose.connect(process.env.DBurl);
 
-        console.log(`\nMongoDB connected !! DB HOST: ${connectionInstance.connection.host}\n`);
+        console.log(`\nMongoDB connected !! DB HOST: ${connectionInstance.connection.host}\n chl gya`);
     } catch (error) {
         console.log("MONGODB connection error", error);
         process.exit(1);
     }
 };
 
-export default connectDB;
+export default connectDB;
